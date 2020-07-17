0a4d72a5d1e3182f6eb513d29b2eef408aafe278
cardoso <matheus@cardo.so>
2020-07-16 22:00:07 -0300

    Support Carthage & Bump 0.1.5

---
+++ Streamoji/Streamoji.h
@@ -0,0 +1,19 @@
+//
+//  Streamoji.h
+//  Streamoji
+//
+//  Created by Matheus Cardoso on 16/07/20.
+//  Copyright © 2020 Matheus Cardoso. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Streamoji.
+FOUNDATION_EXPORT double StreamojiVersionNumber;
+
+//! Project version string for Streamoji.
+FOUNDATION_EXPORT const unsigned char StreamojiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Streamoji/PublicHeader.h>
+
+
